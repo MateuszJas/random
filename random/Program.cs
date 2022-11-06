1c8ace81730000b20efd7517f20e6ec8e5419994
MateuszJas <pphu.jaskolka@hotmail.com>
2022-11-06 11:05:22 +0100

    Dodaj pliki projektów.

---
+++ random/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random losuj = new Random();
+            int x, a;
+            x = losuj.Next(10); 
+            Console.WriteLine("Liczba wylosowana, zgaduj!");
+
+            do
+            {
+                Console.WriteLine("Podaj swoją liczbę");
+                a = System.Convert.ToInt16(Console.ReadLine());
+
+                if (a > x)
+                    Console.WriteLine("Za dużo!");
+                if (a < x)
+                    Console.WriteLine("Za mało!");
+
+            } while (a != x);
+
+            Console.WriteLine("Wygrałeś");
+            Console.ReadKey();  
+        }
+          
+    }
+}
